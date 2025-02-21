0899f96310bd48b4b54edc92dca6a196ed5c48a9
Messidinesh12 <dineshkumarramesh12@gmail.com>
2025-02-21 18:49:28 +0530

    changes

--- src/participant/participant.module.ts
+++ src/participant/participant.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ParticipantService } from './participant.service';
 import { ParticipantController } from './participant.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { EventService } from 'src/event/event.service';
+import { EventModule } from 'src/event/event.module';
+import { VenueModule } from 'src/venue/venue.module';
 
 @Module({
+  imports: [EventModule, VenueModule],
   controllers: [ParticipantController],
-  providers: [ParticipantService],
+  providers: [ParticipantService, PrismaService],
 })
 export class ParticipantModule {}

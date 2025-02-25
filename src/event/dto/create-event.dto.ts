8b6834536ddd0c91e33ebeded9192457d7793fd2
Messidinesh12 <dineshkumarramesh12@gmail.com>
2025-02-25 18:36:01 +0530

    event input changes

--- src/event/dto/create-event.dto.ts
+++ src/event/dto/create-event.dto.ts
@@ -6,6 +6,7 @@ import {
   IsNotEmpty,
   IsOptional,
   IsString,
+  MinDate,
 } from 'class-validator';
 
 export class CreateEventDto {
@@ -27,24 +28,31 @@ export class CreateEventDto {
 
   @IsOptional()
   @IsDateString()
+  @MinDate(new Date(), { message: 'Date must be today or in the future' })
   date: string; // Required for SINGLE_DAY
 
   @IsOptional()
   @IsArray()
   @IsDateString()
+  @MinDate(new Date(), {
+    each: true,
+    message: 'Each custom date must be today or later',
+  })
   customDates: string[]; // For CUSTOM_DATE events
 
   @IsOptional()
   @IsArray()
-  @IsDateString()
+  @IsString({ each: true })
   recurringDays: string[]; // For RECURRING events (e.g., ["Monday", "Wednesday"])
 
   @IsOptional()
   @IsDateString()
+  @MinDate(new Date(), { message: 'Start date must be today or in the future' })
   startDate: string; // For SEASONAL, ALL_DAYS
 
   @IsOptional()
   @IsDateString()
+  @MinDate(new Date(), { message: 'End date must be today or in the future' })
   endDate: string; // For SEASONAL, ALL_DAYS
 
   @IsOptional()
